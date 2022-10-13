cc560637d84b18716e06391fe9e2c0279ce162d5
SebastianJaureguiM <sebastian.jaureguimaldonado@gmail.com>
2022-10-13 17:59:12 -0500

    update 4

--- src/createUser.js
+++ src/createUser.js
@@ -1,10 +1,26 @@
 const {v4} = require('uuid')
 const AWS = require('aws-sdk')
+const parser = require('lambda-multipart-parser')
 
 module.exports.main = async (event) => {
-    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const dynamodb = new AWS.DynamoDB.DocumentClient()
+    const s3 = new AWS.S3()
 
-    const data = JSON.parse(event.body)
+    const data = await parser.parse(event)
+
+    const { content, filename, contentType } = data.files[0]
+    const params = {
+        Bucket: "bucket-prueba-parking",
+        Key: filename,
+        Body: content,
+        ContentDisposition: `attachment; filename="${filename}";`,
+        ContentType: contentType,
+        ACL: "public-read"
+    };
+    
+    await s3.upload(params).promise()
+    
+    /*
     const createdAt = new Date()
     const id = v4()
     
@@ -18,9 +34,9 @@ module.exports.main = async (event) => {
         TableName: "users_parking",
         Item: newUser,
     }).promise();
-    
+    */
     return {
         statusCode:200,
-        body: JSON.stringify(newUser)
+        body: JSON.stringify({result})
     }
 }
--- src/getImageUser.js
+++ src/getImageUser.js
@@ -2,16 +2,30 @@ const AWS = require('aws-sdk')
 
 module.exports.main = async (event) => {
     const s3 = new AWS.S3()
-    
     const bucketParams = {
-        Bucket: 'bucket-prueba-parking',
+        Bucket: 'bucket-prueba-parking'
     }
 
     const result = await s3.listObjects(bucketParams).promise()
+    const contents = result.Contents
 
+    const listUrl = []
+    contents.forEach(element => {
+        if (element.Key.includes(".")) {
+            const url = `https://${bucketParams.Bucket}.s3.us-east-2.amazonaws.com/${element.Key}`
+            listUrl.push(url)
+        }
+    })
+
+   /*
+    const params = {Bucket: 'bucket-prueba-parking', Key: '8b167af653c2399dd93b952a48740620.jpg'}
+    const url = s3.getSignedUrl('getObject', params)
+    */
     return {
         statusCode:200,
-        body:JSON.stringify(result)
+        body:JSON.stringify({
+            listUrl
+        })
     }
     
 }
